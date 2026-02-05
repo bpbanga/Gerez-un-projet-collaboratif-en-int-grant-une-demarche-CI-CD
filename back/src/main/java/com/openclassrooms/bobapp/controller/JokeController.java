03af975d806f0cf7eea7ff52456304befae31bf3
bpbanga <97730466+bpbanga@users.noreply.github.com>
2026-02-05 16:01:27 +0100

    Update JokeController.java to param core to authorise front render to use back render

--- back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
+++ back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
@@ -4,11 +4,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.openclassrooms.bobapp.service.JokeService;
 
 @RestController
 @RequestMapping("api/joke")
+@CrossOrigin(origins = "https://bobapp-frontend.onrender.com")
 public class JokeController {
 
     private final JokeService jokeService;
